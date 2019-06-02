e9cd1a7a8464c0179dd90be9117d257a7b05dc7b
Kelvin Wahome <kevowahome@gmail.com>
2019-06-02 20:41:54 +0300

    [wip]: multithreaded web server completed

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -12,8 +12,18 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     let pool = ThreadPool::new(4);
 
+    let mut counter = 0;
+
     for stream in listener.incoming() {
+        if counter == 10 {
+            println!("Shutting down.");
+            break;
+        }
+
+        counter += 1;
+
         let stream = stream.unwrap();
+
         pool.execute(|| {
             handle_connection(stream);
         });
